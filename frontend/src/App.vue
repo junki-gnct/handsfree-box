66ec2d1819aeb4e7eaf791a7f8128ce8839790d7
Junki Tomatsu <me@junki-t.net>
2020-10-16 10:35:29 +0900

    Create project and setup actions. (+11 squashed commits) Squashed commits: [99e6e45] Change Github Actions Trigger timings [6573a65] Review Dog Warning Test. [d3f4536] Update reviewdog.yml [8fb86c7] Add reviewdog [c2010c8] Lint warnings test. [13f1a54] Add eslint config [570223c] Add Github Actions [aa4db17] Commit some files [d85e16d] Remove eslintcache [17ed268] Remove Unused files. [4920978] Create frontend project

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Header />
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <router-view></router-view>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import Header from './components/Header.vue';
+
+@Component({
+  components: {
+    Header,
+  },
+})
+export default class App extends Vue {}
+</script>
