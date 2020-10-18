abee0f6b320918d4555fbede06566316e0270f4b
Junki Tomatsu <me@junki-t.net>
2020-10-18 23:03:00 +0900

    解錠状況の読み込み機能の実装

--- device/src/index.ts
+++ device/src/index.ts
@@ -1,18 +1,26 @@
 import * as firebase from 'firebase-admin';
 import * as serviceAdminCredentials from './miscs/serviceAccountCredentials.json';
 
+console.log(`Handsfree Box v${(process.env.npm_package_version as string)}`);
+console.log('[Firebase] Connecting to database...');
 firebase.initializeApp({
   credential: firebase.credential.cert({
     clientEmail: serviceAdminCredentials.client_email,
     privateKey: serviceAdminCredentials.private_key,
-    projectId: serviceAdminCredentials.project_id
+    projectId: serviceAdminCredentials.project_id,
   }),
-  databaseURL: 'https://handsfree-box.firebaseio.com'
+  databaseURL: 'https://handsfree-box.firebaseio.com',
 });
 
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+let isFirstRun = false;
+const db = firebase.database();
+const ref = db.ref('/');
 
-console.log(serviceAdminCredentials.type);
-console.log(hello('TypeScript!!!!!'));
+void ref.on('value', (snapshot) => {
+  if(!isFirstRun) {
+    console.log('[Firebase] Connected.');
+    isFirstRun = true;
+  }
+  const state = snapshot.child('isOpen').val() as boolean;
+  console.log(state);
+});
