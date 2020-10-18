4fea0c37faed1f0d450f6710957cd7cf507a8a8e
Junki Tomatsu <me@junki-t.net>
2020-10-18 22:35:38 +0900

    デバイスにFirebaseSDKの追加

--- device/src/index.ts
+++ device/src/index.ts
@@ -1,5 +1,18 @@
+import * as firebase from 'firebase-admin';
+import * as serviceAdminCredentials from './miscs/serviceAccountCredentials.json';
+
+firebase.initializeApp({
+  credential: firebase.credential.cert({
+    clientEmail: serviceAdminCredentials.client_email,
+    privateKey: serviceAdminCredentials.private_key,
+    projectId: serviceAdminCredentials.project_id
+  }),
+  databaseURL: 'https://handsfree-box.firebaseio.com'
+});
+
 function hello(name: string): string {
   return `Hello, ${name}!`;
 }
 
+console.log(serviceAdminCredentials.type);
 console.log(hello('TypeScript!!!!!'));
