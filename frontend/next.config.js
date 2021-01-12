adf654b8e5b7db1d4992a06921df215e2d0540ee
Junki Tomatsu <me@junki-t.net>
2021-01-12 12:06:46 +0900

    Vue.js → Next.jsに変更

---
+++ frontend/next.config.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withSass = require('@zeit/next-sass');
+
+module.exports = withSass({});
---
+++ frontend/src/components/atomos/Hello.tsx
@@ -0,0 +1,11 @@
+import './Hello.scss';
+
+const Hello: React.FunctionComponent = () => (
+  <div>
+    <h1 className="test">
+      Hello <h2 className="test__dev">World!</h2>
+    </h1>
+  </div>
+);
+
+export default Hello;
